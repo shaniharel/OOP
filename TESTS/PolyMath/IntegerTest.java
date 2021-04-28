e33ef2d33118d18e6ebd3940f502abd6084d1066
shani <hareldva@post.bgu.ac.il>
2021-04-28 18:26:37 +0300

    k

--- TESTS/PolyMath/IntegerTest.java
+++ TESTS/PolyMath/IntegerTest.java
@@ -7,9 +7,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntegerTest {
-
+    private Integer n1;
+    private Integer n2;
+    private Integer n3;
+    private Integer n4;
     @BeforeAll
     void setUp() {
+        n1=new Integer(0);
+        n2=new Integer(1);
+        n3=new Integer(1000);
+        n4=new Integer(0);
     }
 
     @Test
